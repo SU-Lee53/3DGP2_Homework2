b2fca86ddd98ed77a3c49ba0493478908d589c26
SU-Lee53 <dltmddnr05@naver.com>
2025-11-12 15:25:16 +0900

    Effect in progress

--- hw2/Effect.cpp
+++ hw2/Effect.cpp
@@ -1,2 +1,29 @@
 #include "stdafx.h"
 #include "Effect.h"
+
+void ExplosionEffect::Create(ComPtr<ID3D12Device> pd3dDevice, ComPtr<ID3D12GraphicsCommandList> pd3dCommandList, int nParticles)
+{
+	std::vector<ParticleVertexType> vertices(nParticles);
+	for (int i = 0; i < vertices.size(); ++i) {
+		vertices[i].xmf3Position = XMFLOAT3(0.f, 0.f, 0.f);
+		vertices[i].xmf4Color = XMFLOAT4(1.f, 0.f, 0.f, 0.f);
+		vertices[i].xmf3InitialVelocity.x = RandomGenerator::GenerateRandomFloatInRange(-3.f, 3.f);
+		vertices[i].xmf3InitialVelocity.y = RandomGenerator::GenerateRandomFloatInRange(-3.f, 3.f);
+		vertices[i].xmf3InitialVelocity.z = RandomGenerator::GenerateRandomFloatInRange(-3.f, 3.f);
+
+		vertices[i].xmf2InitialSize.x = vertices[i].xmf2InitialSize.y = RandomGenerator::GenerateRandomFloatInRange(1.f, 10.f);
+		vertices[i].fRandomValue = RandomGenerator::GenerateRandomFloatInRange(0.f, 1.f);
+		vertices[i].fStartTime = 0.f;
+		vertices[i].fLifeTime = RandomGenerator::GenerateRandomFloatInRange(0.5f, 3.f);
+		vertices[i].fMass = RandomGenerator::GenerateRandomFloatInRange(1.f, 2.f);
+
+		m_fTotalLifetime = std::max(m_fTotalLifetime, vertices[i].fLifeTime);
+	}
+
+	m_pd3dParticleBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, vertices.data(), vertices.size() * sizeof(ParticleVertexType),
+		D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, m_pd3dParticleUploadBuffer.GetAddressOf());
+	d3dVertexBufferView.BufferLocation = m_pd3dParticleBuffer->GetGPUVirtualAddress();
+	d3dVertexBufferView.StrideInBytes = sizeof(ParticleVertexType);
+	d3dVertexBufferView.SizeInBytes = sizeof(ParticleVertexType) * nParticles;
+
+}
--- hw2/Effect.h
+++ hw2/Effect.h
@@ -1,16 +1,44 @@
 #pragma once
+
+struct CB_PARTICLE_DATA {
+	XMFLOAT3 xmf3Position;
+	XMFLOAT3 xmf3Force;
+	float fTotalTime;
+	float m_fTotalLifetime;
+};
+
+struct ParticleVertexType {
+	XMFLOAT3 xmf3Position;
+	XMFLOAT4 xmf4Color;
+	XMFLOAT3 xmf3InitialVelocity;
+	XMFLOAT2 xmf2InitialSize;
+	float fRandomValue;
+	float fStartTime;
+	float fLifeTime;
+	float fMass;
+};
+
 class Effect {
 public:
+	Effect() {}
 
+	virtual void Create(ComPtr<ID3D12Device> pd3dDevice, ComPtr<ID3D12GraphicsCommandList> pd3dCommandList, int nParticles) {}
 
-private:
+protected:
+	ComPtr<ID3D12Resource> m_pd3dParticleBuffer;
+	ComPtr<ID3D12Resource> m_pd3dParticleUploadBuffer;
 
+	UINT nParticles = 0;
+	D3D12_VERTEX_BUFFER_VIEW d3dVertexBufferView;
+
+	float m_fTotalLifetime = 0.f;
 
 };
 
 class ExplosionEffect : public Effect {
 public:
-
+	ExplosionEffect() {}
+	virtual void Create(ComPtr<ID3D12Device> pd3dDevice, ComPtr<ID3D12GraphicsCommandList> pd3dCommandList, int nParticles) override;
 
 private:
 
